30eae9e735bdc0e0509d98b06f092d98c12d4df0
Peter Irlam <irlampeter@gmail.com>
2020-11-06 17:32:54 +0000

    Add variables

---
+++ app.js
@@ -0,0 +1,11 @@
+let p1Btn = document.querySelector('#p1Btn');
+let p2Btn = document.querySelector('#p2Btn');
+let resetBtn = document.querySelector('#reset');
+let p1Disp = document.querySelector('#p1Disp');
+let p2Disp = document.querySelector('#p2Disp');
+let winningScoreSelect = document.querySelector('#playTo');
+
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
+let gameOver = false;
