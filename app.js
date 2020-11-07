7f2a729e43fe501454e1f67e0558946cf95cea31
Peter Irlam <irlampeter@gmail.com>
2020-11-07 17:38:57 +0000

    Add player one & player two button logic

--- app.js
+++ app.js
@@ -9,3 +9,27 @@ let p1Score = 0;
 let p2Score = 0;
 let winningScore = 5;
 let gameOver = false;
+
+p1Btn.addEventListener('click', function() {
+  if (!gameOver) {
+    p1Score += 1;
+    if (p1Score === winningScore) {
+      gameOver = true; //also runs if gameOver = !gameOver
+      p1Disp.classList.add('winner');
+      p2Disp.classList.add('loser');
+    }
+    p1Disp.textContent = p1Score;
+  }
+});
+
+p2Btn.addEventListener('click', function() {
+  if (!gameOver) {
+    p2Score += 1;
+    if (p2Score === winningScore) {
+      gameOver = true; //also runs if gameOver = !gameOver
+      p2Disp.classList.add('winner');
+      p1Disp.classList.add('loser');
+    }
+    p2Disp.textContent = p2Score;
+  }
+});
